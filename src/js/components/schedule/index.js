18a625fa37929c3b72f45a8623d383cfbac9a6f2
re54k <ponyowa@163.com>
2016-03-28 18:24:11 +0800

    events

--- src/js/components/schedule/index.js
+++ src/js/components/schedule/index.js
@@ -14,6 +14,81 @@ class Schedule extends Component {
 			<div styleName="page">
 				<Filter/>
 				<Datepicker/>
+
+				<main>
+					<section styleName="panel">
+						<div styleName="tag-wrapper">
+							<div styleName="tag">精选赛程</div>
+						</div>
+						<ul styleName="list">
+							<li styleName="list__item">
+								<div styleName="event">
+									<div styleName="clip-group">
+										<div styleName="clip--red">
+											<i styleName="clip--red__handle"/>
+											<div styleName="clip--red__entity">中国</div>
+										</div>
+										<div styleName="clip--yellow">
+											<i styleName="clip--yellow__handle"/>
+											<div styleName="clip--yellow__entity">决赛</div>
+										</div>
+									</div>
+									<div styleName="event__time">18:30</div>
+									<ul styleName="rows">
+										<li styleName="tags">
+											<div styleName="tags__discipline">游泳</div>
+											<div styleName="stat-wrapper">
+												<div styleName="stat--live">
+													<i styleName="stat--live__icon"/>
+													<span styleName="stat--live__txt">正在直播</span>
+												</div>
+											</div>
+										</li>
+										<li>女子双打第2轮/1/4决赛</li>
+										<li styleName="competition">
+											<div styleName="competition__rival">
+												<span styleName="competition__rival__nation">中国</span>
+												<img styleName="competition__rival__flag" src="/mocks/pic/flag1.png"/>
+											</div>
+											<div styleName="competition__score">vs</div>
+											<div styleName="competition__rival">
+												<img styleName="competition__rival__flag" src="/mocks/pic/flag2.png"/>
+												<span styleName="competition__rival__nation">中国</span>
+											</div>
+										</li>
+									</ul>
+								</div>
+
+								<ul styleName="matches">
+									<li styleName="match">
+										<div>13:00 - 13:10</div>
+										<div styleName="match__group">第1组</div>
+										<div styleName="competition">
+											<div styleName="competition__rival">
+												<span styleName="competition__rival__nation">中国</span>
+												<img styleName="competition__rival__flag" src="/mocks/pic/flag1.png"/>
+											</div>
+											<div styleName="competition__score">vs</div>
+											<div styleName="competition__rival">
+												<img styleName="competition__rival__flag" src="/mocks/pic/flag2.png"/>
+												<span styleName="competition__rival__nation">中国</span>
+											</div>
+										</div>
+									</li>
+									<i styleName="matches__sharp"/>
+								</ul>
+							</li>
+						</ul>
+					</section>
+					<section styleName="panel">
+						<div styleName="tag-wrapper">
+							<div styleName="tag">14/<small>8月</small> 精选赛程</div>
+						</div>
+					</section>
+				</main>
+
+
+
 				{this.props.onlyChina && '中国'} /
 				{this.props.onlyFinal && '决赛'} /
 				{this.props.selectedDiscipline && this.props.selectedDiscipline.name} /
